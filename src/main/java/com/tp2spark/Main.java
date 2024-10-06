ec7863310e79ccd5857d12c17bf2580a17b74837
micaela alemany <alemanymicaela@gmail.com>
2024-10-06 11:44:43 -0300

    Mensaje de los cambios

--- src/main/java/com/tp2spark/Main.java
+++ src/main/java/com/tp2spark/Main.java
@@ -18,7 +18,7 @@ public class Main {
             System.err.println("No se pudo conectar a la base de datos: " +
                     e.getMessage());
         }
-        get("/materias-primas", MateriaPrimaControlador.getTodasLasMateriasPrimas);
+        // get("/materias-primas", MateriaPrimaControlador.getTodasLasMateriasPrimas);
         // post("/materias-primas", MateriaPrimaControlador.insertar_materiaP);
     }
 };
